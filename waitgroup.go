37368ec33decdaa6cc7c02820d74fc0ca4fb903c
胡成红 <huchenghong@yuanben.org>
2021-11-02 12:02:58 +0800

    refactor: 添加方法注释

--- waitgroup.go
+++ waitgroup.go
@@ -9,7 +9,7 @@ type limitWaitGroup struct {
 	concurrent chan struct{}
 }
 
-// 初始化
+// NewWaitGroup 初始化
 func NewWaitGroup(size int32) *limitWaitGroup {
 	if size <= 0 {
 		size = 1
@@ -19,19 +19,19 @@ func NewWaitGroup(size int32) *limitWaitGroup {
 	}
 }
 
-// 计数加1
+// Add 计数加1
 func (lwg *limitWaitGroup) Add() {
 	lwg.concurrent <- struct{}{}
 	lwg.wg.Add(1)
 }
 
-// 计数减1
+// Done 计数减1
 func (lwg *limitWaitGroup) Done() {
 	<-lwg.concurrent
 	lwg.wg.Done()
 }
 
-// 阻塞
+// Wait 阻塞
 func (lwg *limitWaitGroup) Wait() {
 	lwg.wg.Wait()
 }
